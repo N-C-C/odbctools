861bb64287198445aae19b2833d392d835d53c53
Andrew C. Yatsko <ayatsko@northampton.edu>
2017-05-08 15:51:55 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='odbctools',
+    version='0.1',
+    packages=['tests', 'odbcmanager'],
+    url='https://github.com/N-C-C/odbctools',
+    license='',
+    author='Andrew Yatsko',
+    author_email='ayatsko@live.com',
+    description='A set of tools to simplify connecting to and working with an ODBC data source'
+)
