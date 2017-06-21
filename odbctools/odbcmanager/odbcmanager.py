ccea967a532a83ef527b8a19e1188a73a2537951
jglasgow3 <jglasgow@northampton.edu>
2017-06-21 13:05:05 -0400

    Add ability to run no result sets, through (query_no_resultset).  Adds auto_commit parameter.

--- odbctools/odbcmanager/odbcmanager.py
+++ odbctools/odbcmanager/odbcmanager.py
@@ -4,13 +4,14 @@ from configparser import ConfigParser
 
 
 class OdbcManager:
-    def __init__(self):
+    def __init__(self, auto_commit=False):
         config = ConfigParser()
         config.read('config.ini')
         self.connection_name = config.get('odbc', 'DSN')
+        self.auto_commit = auto_commit
 
     def __enter__(self):
-        self.__conn = odbc.connect('DSN={0};'.format(self.connection_name))
+        self.__conn = odbc.connect('DSN={0};'.format(self.connection_name),self.auto_commit)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -49,7 +50,7 @@ class OdbcManager:
         Queries data source
         :param query_string: The query to execute
         :param params: Parameters to pass to query
-        :return: A list that contains (0)A list containing the query columns (1) A 
+        :return: A list that contains (0)A list containing the query columns (1) A list of the resulting data (rows)
         """
         cur = self.__conn.cursor()
         cur.execute(query_string, params)
@@ -58,3 +59,25 @@ class OdbcManager:
         for row in cur.fetchall():
             rows.append([col.rstrip(' ') if type(col) is str else col for col in row])
         return [header, rows]
+
+    def query_no_resultset(self, query_string, params=None, commit=None):
+        """
+        For running DDL/DML queries for creating, deleting, updating database objects.
+        :param query_string: The query to execute
+        :param params: Parameters to pass to query
+        :param commit: If option is used, does not update database, but still use caution
+        :return: Message indicating transaction was committed\n
+                True = Was committed\n
+                False = Not committed
+        """
+        cur = self.__conn.cursor()
+        cur.execute(query_string, params)
+
+        if not commit:
+            return False
+        elif not self.auto_commit:
+            return False
+        else:
+            cur.commit()
+            return True
+
